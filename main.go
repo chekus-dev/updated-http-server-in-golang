b59dbfe6e74a7be062bf61fed9b4ab415fc7c214
chekus-dev <okaforchekus@gmail.com>
2026-06-01 13:48:31 +0100

    Implement JSON response in HomePage
    Added JSON response handling to the HomePage function.

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"html/template"
+	"encoding/json"
 )
+
+type  UserResponse struct {
+	Message string `json:"message"`
+	Status int `json:"status"`
+}
+func HomePage(w http.ResponseWriter, res *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	data := UserResponse{
+		Message: "Welcome to updated http server in go with JSON, this is JSON data!",
+		Status:200,
+	} 
+
+	json.NewEncoder(w).Encode(data)
+
+}
+func main(){
+	http.HandleFunc("/", HomePage)
+	fmt.Println("starting updated server on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
