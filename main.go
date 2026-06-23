dbac1e57c8615178fdceb3943224d68eb2bb0ffd
chekus-dev <okaforchekus@gmail.com>
2026-06-23 10:11:34 +0100

    Refactor main.go to update handlers and profiles

--- main.go
+++ main.go
@@ -6,33 +6,56 @@ import (
 	"net/http"
 )
 
-type Message struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Email        string `json:"email"`
-	HouseAddress string `json:"HouseAddress"`
-}
-
-func HomePage(w http.ResponseWriter, res *http.Request) {
-	w.Header().Set("content-Type", "application/json")
-	user := Message{
-		ID:           1,
-		Name:         "chekus",
-		Email:        "chekusjoseph@yahoo.com",
-		HouseAddress: "no 7 mama ramatu avenue",
-	}
-
-	if err := json.NewEncoder(w).Encode(user); err != nil {
-		log.Printf("failed to encode user: %v", err)
-	}
-}
-
 func main() {
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/user", HomePage)
-	log.Println("starting server")
+	mux.HandleFunc("/user", homeHandler)
+	mux.HandleFunc("/person", personHanlder)
+	log.Println("starting the server on port:8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatalf("failed to start application/json: %v", err)
+		log.Fatalf("failed to start server: %v", err)
 	}
+
+}
+
+func personHanlder(w http.ResponseWriter, res *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+
+	coding := personProfile{
+		ID:    2,
+		Name:  "joseph",
+		Email: "okaforchekus@gmail.com",
+	}
+	if err := json.NewEncoder(w).Encode(coding); err != nil {
+		log.Printf("failed to encode coding: %v", err)
+	}
+}
+
+
+type personProfile struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+
+func homeHandler(w http.ResponseWriter, res *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+
+	data := userProfile{
+		ID:    1,
+		Name:  "chekus",
+		Age:   26,
+		Email: "chekusjoseph@gmail.com",
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Printf("failed to encode data: %v", err)
+
+	}
+}
+
+type userProfile struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
 }
